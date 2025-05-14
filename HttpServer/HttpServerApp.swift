8f517cea832381f9922b54473e380dab2c63cb69
PangChong <pangchongcyc@gmail.com>
2025-05-14 16:03:32 +0800

    Initial commit for checkpoints

---
+++ HttpServer/HttpServerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HttpServerApp.swift
+//  HttpServer
+//
+//  Created by pc on 2025/5/14.
+//
+
+import SwiftUI
+
+@main
+struct HttpServerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
